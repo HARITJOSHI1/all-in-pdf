1927c4944610ebe4e6da72ff83b68bec4d3c0ee2
harit <joshiharit3@gmail.com>
2022-03-18 14:33:42 +0530

    some changes

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const pdfRoute = require('./routes/pdfRoute');
 const cookieParser = require('cookie-parser');
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '/views'));
+
+app.use('/api/pdf', pdfRoute);
 
 if(process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
